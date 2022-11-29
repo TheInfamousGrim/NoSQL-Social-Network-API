cfa859a8193ec8bc888491fc0d4e5c98612be99c
TheInfamousGrim <finchergeorge1@gmail.com>
2022-11-29 17:15:19 +0000

    Add thoughtController functions
    Created all the thought controllers that will facillitate all the CRUD
    operations for the thought data.
    
    These operations are as follows:
        - getThoughts: gets all thoughts that have been posted to the db.
        - getThoughtById: get's a single thought from it's mongodb id.
        - createThought: creates a new thought, fairly straightforward.
        - updateThought: updates an already existing thought that user has
        created.
        - deleteThought: deletes a specified thought
        - createReaction: gets the id of the thought a user wishes to react
        to and appends a reaction in the reaction array in the db.
        - deleteReaction: Again fairly straightforward, deletes a specific
        reaction using thoghtId and the reactionId to find it.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,82 @@
+// Import data models for users, thoughts and reactions
+const { User, Thought } = require('../models');
+
+/* -------------------------------- thoughts -------------------------------- */
+
+// GET all thoughts
+function getThought(req, res) {
+    Thought.find({})
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
+}
+
+// GET a single thought by id
+function getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+        .select('-__v')
+        .then((thought) =>
+            !thought ? res.status(404).json({ message: 'No Thought find with this ID!' }) : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+}
+
+// Create a thought
+function createThought(req, res) {
+    Thought.create(req.body)
+        .then(({ _id }) => User.findOneAndUpdate({ _id: req.body.userId }, { $push: { thoughts: _id } }, { new: true }))
+        .then((thought) =>
+            !thought ? res.status(404).json({ message: 'There is no user with this ID!' }) : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+}
+
+// Delete a thought
+function deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+        .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: 'No thought find with this ID!' })
+                : User.findOneAndUpdate(
+                      { thoughts: req.params.thoughtId },
+                      { $pull: { thoughts: req.params.thoughtId } },
+                      { new: true }
+                  )
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'Thought deleted, but no user found' })
+                : res.json({ message: 'Thought successfully deleted' })
+        )
+        .catch((err) => res.status(500).json(err));
+}
+
+/* -------------------------------- reactions ------------------------------- */
+
+// Create a reaction
+function createReaction(req, res) {
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+    )
+        .then((thought) =>
+            !thought ? res.status(404).json({ message: 'No thought frind with ID!' }) : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+}
+
+// Delete a reaction
+function deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+    )
+        .then((thought) =>
+            !thought ? res.status(404).json({ message: 'No thought find with this ID!' }) : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+}
+
+// Export all controller functions
+module.exports = { getThought, getThoughtById, createThought, deleteThought, createReaction, deleteReaction };
